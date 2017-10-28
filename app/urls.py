d06ceff6e17addd6e075b177b47faa7ee91db27d
Pratiksha96 <jainpratiksha2110@gmail.com>
2017-10-29 03:00:04 +0530

    Added views to create/edit blog post inside the site.

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.todo_list, name='todo_list'),
     url(r'^task/(?P<pk>\d+)/$', views.task_detail, name='task_detail'),
+    url(r'^todo/new/$', views.todo_new, name='todo_new'),
+    url(r'^todo/(?P<pk>\d+)/edit/$', views.todo_edit, name='todo_edit'),
+
 ]
--- app/views.py
+++ app/views.py
@@ -34,6 +34,9 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Todo
 from django.shortcuts import render, get_object_or_404
+from .forms import TodoForm
+from django.shortcuts import redirect
+
 
 def todo_list(request):
     tasks = Todo.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
@@ -42,3 +45,28 @@ def todo_list(request):
 def task_detail(request, pk):
     task = get_object_or_404(Todo, pk=pk)
     return render(request, 'app/task_detail.html', {'task': task})
+
+def todo_new(request):
+    if request.method == "POST":
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.created_date = timezone.now()
+            task.save()
+            return redirect('task_detail', pk=task.pk)
+    else:
+        form = TodoForm()
+    return render(request, 'app/todo_edit.html', {'form': form})
+
+def todo_edit(request, pk):
+    task = get_object_or_404(Todo, pk=pk)
+    if request.method == "POST":
+        form = TodoForm(request.POST, instance=task)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.created_date = timezone.now()
+            task.save()
+            return redirect('task_detail', pk=task.pk)
+    else:
+        form = TodoForm(instance=task)
+    return render(request, 'app/todo_edit.html', {'form': form})
