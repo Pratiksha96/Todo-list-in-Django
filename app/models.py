84f5d7d4dd47ad4c3d7ced9c5bed446d585bf9c3
Pratiksha96 <jainpratiksha2110@gmail.com>
2017-11-05 23:03:42 +0530

    Improved CSS.

--- app/models.py
+++ app/models.py
@@ -8,8 +8,8 @@ from django.utils import timezone
 class Todo(models.Model):
     #todo_job = models.TextField()
     #created_date = models.DateTimeField(editable=False , default=datetime.now())
-    #author = models.ForeignKey('auth.User')
-    title = models  .CharField(max_length=200)
+    #name = models.ForeignKey('auth.User',null=True)
+    title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
 
